2730b7d27c5851172d5dd68901b9b9b9859e73af
Nicollas Matheus <127448248+NicollasMPP@users.noreply.github.com>
2023-03-13 00:21:16 -0300

    mais treino

---
+++ exercicios/interactive-rating/js/scripts.js
@@ -0,0 +1,6 @@
+function clique() {
+    var botao1 = document.querySelector('#notas button')
+    if (botao1)
+    document.querySelector('#notas button').style.backgroundColor = "hsl(25, 97%, 53%)"
+    document.querySelector('#notas button')
+}
