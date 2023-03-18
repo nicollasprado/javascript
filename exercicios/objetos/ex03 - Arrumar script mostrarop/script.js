1b7dd461f0c466950382a30e1a5fab5ab6f08d83
Nicollas Matheus <127448248+NicollasMPP@users.noreply.github.com>
2023-03-18 00:53:29 -0300

    atualizacao do exercicio

--- exercicios/objetos/ex03 - Arrumar script mostrarop/script.js
+++ exercicios/objetos/ex03 - Arrumar script mostrarop/script.js
@@ -8,6 +8,8 @@ const radioCivil = document.getElementById('radioCivil');
 const tipo = document.getElementById('Tipo');
 const divRes = document.getElementById('resultado');
 const btnAdd = document.getElementById('btnAdd');
+const resBtns = document.getElementById('resBtns');
+const btnRemover = document.getElementById('btnRemover');
 
 class Carro{
     constructor(par_nome, par_portas){
@@ -15,7 +17,6 @@ class Carro{
         this.portas=par_portas;
     }
 }
-
 class Militar extends Carro{
     constructor(par_nome, par_portas, par_blindagem, par_municao){
         super(par_nome, par_portas);
@@ -25,49 +26,50 @@ class Militar extends Carro{
 }
 
 // Evento Criar Item
-
 const criarItem = (par_tipo, par_nome, par_portas, par_blindagem, par_municao)=>{
     let restxt = document.createElement('div');
+    let restxtp = document.createElement('p')
     restxt.classList.add('novoResultado');
     divRes.appendChild(restxt);
+    restxt.appendChild(restxtp);
     if(par_tipo == 'Militar') {
-        restxt.innerHTML = `Nome: <strong>${par_nome}</strong><br>Tipo: <strong>${par_tipo}</strong><br>Portas: <strong>${par_portas}</strong><br>Blindagem: <strong>${par_blindagem}</strong><br>Munição: <strong>${par_municao}</strong><br>`;
+        restxtp.innerHTML = `Nome: <strong>${par_nome}</strong><br>Tipo: <strong>${par_tipo}</strong><br>Portas: <strong>${par_portas}</strong><br>Blindagem: <strong>${par_blindagem}</strong><br>Munição: <strong>${par_municao}</strong><br>`;
     } else if(par_tipo == 'Civil') {
-        restxt.innerHTML = `Nome: <strong>${par_nome}</strong><br>Tipo: <strong>${par_tipo}</strong><br>Portas: <strong>${par_portas}`;
+        restxtp.innerHTML = `Nome: <strong>${par_nome}</strong><br>Tipo: <strong>${par_tipo}</strong><br>Portas: <strong>${par_portas}`;
     }
-}
-
-// Evento Mostrar Opções - Arrumar!
-
-let mostrarOpcoes = ()=>{
-    let resitem = [...document.querySelectorAll('.novoResultado')];
-    resitem.map((item)=>{
-    item.addEventListener('click', ()=>{
-        let divOpcoes = document.createElement('div');
-        divOpcoes.classList.add('divOpcoes')
-        let remover = document.createElement('p');
-        remover.classList.add('btnRemover')
-        let modificar = document.createElement('p');
-        modificar.classList.add('btnModificar')
-        remover.innerText = 'REMOVER';
-        modificar.innerText = 'MODIFICAR';
-        // Testa se ja existe uma div de opções
-
-        // CONSERTAR DEPOIS
-        if(item.childElementCount === 11) {
-            itemNovo = item.lastChild;
-            item.removeChild(itemNovo);
-        }else {
-            divOpcoes.appendChild(remover);
-            divOpcoes.appendChild(modificar);
-            item.appendChild(divOpcoes);
-        }
-    })
+    // Evento ao clicar na caixa do item criado
+    restxt.addEventListener('click', ()=>{
+    restxt.classList.toggle('clicado');
 })
 }
 
-// Evento de click no botão adicionar
+// Botao remover funcionalidades
 
+btnRemover.addEventListener('click', ()=>{
+    let clicado = [...document.querySelectorAll('.clicado')];
+    if(clicado.length > 1) {
+        alert('Só é possível remover um item por vez');
+        clicado.map((elemento_clicado)=>{
+            elemento_clicado.classList.toggle('clicado');
+        })
+    }else {
+        if(confirm('Tem certeza?') == true){
+            clicado.map((elemento_clicado)=>{
+                divRes.removeChild(elemento_clicado);
+                let carrosCriados = [...document.querySelectorAll('.novoResultado')]
+                if(carrosCriados.length <= 0) {
+                    resBtns.classList.remove('aparecer');
+                }
+            })
+        }else{
+            clicado.map((elemento_clicado)=>{
+                elemento_clicado.classList.toggle('clicado');
+            })
+        }
+    }
+})
+
+// Evento de click no botão adicionar
 const carros = []
 const carMilitares = []
 
@@ -93,7 +95,6 @@ btnAdd.addEventListener('click', ()=>{
             carros.push(militar);
             carMilitares.push(militar);
             criarItem('Militar', nome, portas, blindagem, municao);
-            mostrarOpcoes();
         }
     }
     if(radioCivil.checked){
@@ -105,25 +106,28 @@ btnAdd.addEventListener('click', ()=>{
             let civil = new Carro(nome, portas);
             carros.push(civil);
             console.log(carros);
-            criarItem('Civil', nome, portas)
+            criarItem('Civil', nome, portas);
         }
+    }
+    // Aparição dos botões de remover e modificar
+    if(carros.length != 0) {
+        resBtns.classList.add('aparecer');
     }
 })
 
 // Efeitos ao clicar nos tipos
-
 radioCivil.addEventListener('change', ()=>{
     inputBlindagem.setAttribute('readonly', true);
-    inputBlindagem.classList.remove('militarMarcado')
+    inputBlindagem.classList.remove('militarMarcado');
     inputBlindagem.value = 0;
     inputMunicao.setAttribute('readonly', true);
-    inputMunicao.classList.remove('militarMarcado')
+    inputMunicao.classList.remove('militarMarcado');
     inputMunicao.value = 0;
 })
 
 radioMilitar.addEventListener('change', ()=>{
     inputBlindagem.removeAttribute('readonly');
-    inputBlindagem.classList.add('militarMarcado')
+    inputBlindagem.classList.add('militarMarcado');
     inputMunicao.removeAttribute('readonly');
-    inputMunicao.classList.add('militarMarcado')
+    inputMunicao.classList.add('militarMarcado');
 })
